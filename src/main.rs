7acf07fa00107b9fbb03a85aa9556bedd47df382
PratiyushT <aloomataloo@gmail.com>
2025-12-14 22:31:47 -0600

    Added a TCP listener

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,19 @@
 use std::net::TcpListener;
 
+static ADDRESS: &str = "127.0.0.1:7878"; // <IP>:<Port>
+
 fn main() {
-    println!("Hello, world!");
+    // Start a TCP listener at an ADDRESS.
+    // We are binding (connecting/ listening) to this port.
+    let listener = TcpListener::bind(ADDRESS)
+        .expect(format!("Could not start a TCP listener in {}", ADDRESS).as_str());
+
+    // Each stream represents open connection between client and the server.
+    for stream in listener.incoming() {
+        if let Ok(stream) = stream {
+            println!("{:?}", stream);
+        }
+    }
 }
+
+
